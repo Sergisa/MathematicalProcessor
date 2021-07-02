48446e47cc3d81c0d778f7e89543612a16858f8a
Sergisa <phebra@yandex.ru>
2021-07-02 11:58:00 +0300

    Добавлено определение среднего арифметического через сигму.

--- src/com/company/Mathematics.java
+++ src/com/company/Mathematics.java
@@ -3,15 +3,15 @@ package com.company;
 public class Mathematics {
     private static final int ITERATIONS = 17;
     public static double exp(double x){
-        return sigma(i -> power(x, i) / factorial(i));
+        return sigma(i -> power(x, i) / factorial(i), ITERATIONS);
     }
 
     public static double sin(double x){
-        return sigma(n -> (power(-1, n) * power(x, 2 * n + 1)) / (factorial(2 * n + 1)));
+        return sigma(n -> (power(-1, n) * power(x, 2 * n + 1)) / (factorial(2 * n + 1)), ITERATIONS);
     }
 
     public static double cos(double x){
-        return sigma(n -> (power(-1, n) * power(x, 2 * n)) / (factorial(2 * n)));
+        return sigma(n -> (power(-1, n) * power(x, 2 * n)) / (factorial(2 * n)), ITERATIONS);
     }
 
     public static int factorial(int x){
@@ -23,19 +23,24 @@ public class Mathematics {
         return product(n -> x, power);
     }
 
+    public static double mean(double[] x){
+        return sigma(n -> x[n], x.length)/x.length;
+    }
+
     private static double sigma(IterableExpression expr){
         return sigma(expr, 0);
     }
 
-    private static double sigma(IterableExpression expr, int iterationStart, int iterations){
+    private static double sigma(IterableExpression expr, int iterations){
+        return sigma(expr, iterations, 0);
+    }
+
+    private static double sigma(IterableExpression expr, int iterations, int iterationStart){
         double sum = 0;
         for(int n=iterationStart; n<iterations; n++){
             sum += expr.execute(n);
         }
         return sum;
-    }
-    private static double sigma(IterableExpression expr, int iterationStart){
-        return sigma(expr, iterationStart, ITERATIONS);
     }
 
     private static double product(IterableExpression expr){
@@ -48,7 +53,7 @@ public class Mathematics {
 
     private static double product(IterableExpression expr, int iterations, int iterationStart) {
         double res = 1;
-        for(int n=iterationStart; n<iterations; n++){
+        for(int n = iterationStart; n < iterations; n++){
             res *= expr.execute(n);
         }
         return res;
